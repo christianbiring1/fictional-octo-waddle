7e35aeb971156cf6371c3c534080befa66990f81
christian <christianbiringanine22@gmail.com>
2023-08-20 18:45:11 +0200

    Implement the user ID validation with Joi

--- src/components/user/login.jsx
+++ src/components/user/login.jsx
@@ -1,37 +1,74 @@
 import { useState } from "react";
+import Joi from 'joi-browser';
 import './login.css'
 const UserLogin = () => {
-  const [userId, setUserId] = useState('');
-  const [error, setError] = useState('');
 
-  const handleChange = (e) => {
-    setUserId(e.currentTarget.value)
+  const [account, setAccount] = useState({
+    user_id: "",
+  });
+  const [errors, setErrors] = useState({});
 
-    // console.log(userId);
+  const schema = {
+    user_id: Joi.string().trim().min(10).required().label('ID')
   }
+
+   const validate = () => {
+    const result = Joi.validate(account, schema, { abortEarly: false });
+    if (!result.error) return;
+    const errors = {};
+    for (const item of result.error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  const validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const new_schema = { [name]: schema[name] };
+    const { error } = Joi.validate(obj, new_schema);
+
+    return error ? error.details[0].message : null;
+  };
+
+  const handleChange = ({ currentTarget: input }) => {
+    const { name, value } = input;
+    const error = { ...errors };
+    const errorMessage = validateProperty(input);
+    if (errorMessage) error[name] = errorMessage;
+    else delete error[name];
+    setAccount((prev) => ({ ...prev, [name]: value }));
+
+    setErrors(error);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    // Call the Server
-    console.log('Submitted!')
-  }
+    const errors = validate();
+    setErrors(errors || {});
+    if (errors) return;
+
+    // Call the server
+    console.log('Form submited!');
+    window.location = '/';
+  };
 
   return (
     <div className="user_login">
       <form onSubmit={handleSubmit}>
-        <div className="mb-3">
+        <div className="">
           <label htmlFor="userId" className="form-label">ID number</label>
           <input
             type="text"
-            value={userId}
+            value={account.user_id}
             onChange={handleChange}
             className="form-control"
-            id="userId"
+            name="user_id"
+            id="user_id"
+            autoFocus
           />
-          <div id="emailHelp" className="form-text">We'll never share your ID with anyone else.</div>
+          <div id="emailHelp" className="form-text">We&apos;ll never share your ID with anyone else.</div>
         </div>
-        <button type="submit" className="btn btn-primary">Login</button>
+        {errors.user_id && <div className="text-danger error__message">{errors.user_id}</div>}
+        <button type="submit" className="btn btn-primary mt-3">Login</button>
       </form>
     </div>
   );
