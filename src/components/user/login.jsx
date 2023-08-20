fce72aee61d93f530b33df91cc89f94f0edc3879
christian <christianbiringanine22@gmail.com>
2023-08-20 17:25:10 +0200

    make the userid a controlled element

--- src/components/user/login.jsx
+++ src/components/user/login.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import './login.css'
 const UserLogin = () => {
-  const [userId, setuserId] = useState('');
+  const [userId, setUserId] = useState('');
   const [error, setError] = useState('');
+
+  const handleChange = (e) => {
+    setUserId(e.currentTarget.value)
+
+    // console.log(userId);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
+    // Call the Server
     console.log('Submitted!')
   }
 
@@ -14,11 +21,17 @@ const UserLogin = () => {
     <div className="user_login">
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-      <label htmlFor="userId" className="form-label">ID number</label>
-      <input type="text" className="form-control" id="userId"/>
-      <div id="emailHelp" className="form-text">We'll never share your ID with anyone else.</div>
-    </div>
-    <button type="submit" className="btn btn-primary">Login</button>
+          <label htmlFor="userId" className="form-label">ID number</label>
+          <input
+            type="text"
+            value={userId}
+            onChange={handleChange}
+            className="form-control"
+            id="userId"
+          />
+          <div id="emailHelp" className="form-text">We'll never share your ID with anyone else.</div>
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
       </form>
     </div>
   );
