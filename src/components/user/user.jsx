7b3b22763df30355034d707aaa039cfde24274a1
christian <christianbiringanine22@gmail.com>
2023-09-04 19:52:17 +0200

    Implenting the user UI to see the candidates

--- src/components/user/user.jsx
+++ src/components/user/user.jsx
@@ -1,13 +1,43 @@
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+import { useEffect, useState } from 'react';
+import { getCandidates } from '../services/candidateService';
+
 import './user.css'
 
 const MainPage = () => {
+  const [candidates, setCandidates] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await getCandidates();
+      setCandidates(data);
+    }
+
+    fetchData()
+  }, []);
+
+  console.log(candidates);
   return (
     <div className='user_container'>
-      <AccessAlarmIcon />
-      <ThreeDRotation />
-      This is the main user page
+      <h1>Welcome to Voty!</h1>
+
+      <div>
+        {candidates.map((item) => (
+          <div key={item._id} className="card" style={{width: '18rem'}}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <p className="card-text">{item.name}</p>
+              <p className='card-text'>
+                <span>Position: </span>
+                <span>{item.position.name}</span> 
+              </p>
+              <p className='card-text'>
+                <span>Party: </span>
+                <span>{item.political_party.toUpperCase()}</span>
+              </p>
+           </div>
+           <button type='button' className="btn btn-success">Vote</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
